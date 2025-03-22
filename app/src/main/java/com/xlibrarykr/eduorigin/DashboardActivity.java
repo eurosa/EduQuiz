fb55497a35f1df159a4b8d8b32ddc874b79e3ec6
eurosa <radhasonk.phy@gmail.com>
2025-03-22 17:35:44 +0530

    ok

--- app/src/main/java/com/xlibrarykr/eduorigin/DashboardActivity.java
+++ app/src/main/java/com/xlibrarykr/eduorigin/DashboardActivity.java
@@ -2,32 +2,46 @@ package com.xlibrarykr.eduorigin;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
+import com.xlibrarykr.eduorigin.adapters.BookLibraryAdapter;
 import com.xlibrarykr.eduorigin.featurefragments.BookLibraryFragment;
 import com.xlibrarykr.eduorigin.featurefragments.OnlineQuizFragment;
 import com.xlibrarykr.eduorigin.learningresourcefragments.GeeksForGeeksFragment;
 import com.xlibrarykr.eduorigin.learningresourcefragments.JavaTPointFragment;
 import com.xlibrarykr.eduorigin.learningresourcefragments.TutorialsPointFragment;
 import com.xlibrarykr.eduorigin.learningresourcefragments.W3SchoolFragment;
+import com.xlibrarykr.eduorigin.models.ResponseModelBookLibraryBackup;
 import com.xlibrarykr.eduorigin.registration.SignInActivity;
 import com.google.android.material.navigation.NavigationView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -40,7 +54,7 @@ public class DashboardActivity extends AppCompatActivity {
     private String output;
     private View headerView;
     private AdView adView;
-
+    private String delete_url = "https://timxn.com/ecom/EduOriginAPI/Registration/delete.php";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -131,6 +145,11 @@ public class DashboardActivity extends AppCompatActivity {
                         onLogOut();
                         break;
 
+                    case R.id.delete_account:
+
+                        deleteDialog();
+                        break;
+
 
                     case R.id.menu_bookLibrary:
 
@@ -213,7 +232,114 @@ public class DashboardActivity extends AppCompatActivity {
         startActivity(new Intent(DashboardActivity.this, SignInActivity.class));
         finish();
     }
+    private void deleteDialog()
+    {
+        showDeleteAccountDialog();
+    }
 
+    private void showDeleteAccountDialog() {
+        // Create an AlertDialog.Builder instance
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        // Set the title and message for the dialog
+        builder.setTitle("Delete Account");
+        builder.setMessage("Are you sure you want to delete your account? This action cannot be undone.");
+
+        // Set the positive button (e.g., "Delete")
+        
+        builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Perform the delete account action here
+                deleteAccount();
+            }
+        });
+
+        // Set the negative button (e.g., "Cancel")
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Dismiss the dialog
+                dialog.dismiss();
+            }
+        });
+
+        // Create and show the AlertDialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void deleteAccount() {
+        // Implement the logic to delete the account
+  
+            Thread sendThread = new Thread(() -> {
+                try {
+                    // Create a RequestHandler instance
+                    RequestHandler rh = new RequestHandler();
+                    SharedPreferences sp=getSharedPreferences("credential",MODE_PRIVATE);
+                    String email=sp.getString("email","");
+                    String password=sp.getString("password","");
+                    // Define the request parameters (if any)
+                    HashMap<String, String> param = new HashMap<>();
+                    param.put("email", email);
+                    param.put("password", password);
+
+
+                    // Send the POST request and get the response
+                    String result = rh.sendPostRequest(delete_url, param);
+                    Log.d("result123",result);
+         
+               runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            if (isSuccess(result)) {
+                                // Update UI here
+                                Toast.makeText(getApplicationContext(), "Account deleted", Toast.LENGTH_SHORT).show();
+                                SharedPreferences sp = getSharedPreferences("credential", MODE_PRIVATE);
+                                SharedPreferences.Editor editor = sp.edit();
+                                editor.remove("email");
+                                editor.remove("password");
+                                editor.remove("result");
+                                //editor.remove("jishanc@gmail.com");
+                                //editor.remove("hero@gmail.com");
+                                editor.apply();
+                                startActivity(new Intent(DashboardActivity.this, SignInActivity.class));
+                                finish();
+                            }
+                        }
+                    });
+
+
+                } catch (Exception e) {
+                    // Handle other errors
+                    e.printStackTrace();
+                    //  Toast.makeText(context, "An error occurred", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            // Start the thread
+            sendThread.start();
+        
+        
+     
+    }
+
+    public boolean isSuccess(String response) {
+        Log.d("is_success",response);
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            if (jsonObject.optString("status").equals("true")) {
+                return true;
+            } else {
+
+                return false;
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
     // private void  userExistenceCheck()
    //  {
        //  SharedPreferences sp=getSharedPreferences("credential",MODE_PRIVATE);
